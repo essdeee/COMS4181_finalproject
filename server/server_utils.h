48ed50beedb5e2c0de115a7b94cfe3f1753178f9
fwh217 <francis.w.hogan@gmail.com>
2020-12-12 03:37:44 +0000

    Adding server CA certs

--- server/server_utils.h
+++ server/server_utils.h
@@ -5,5 +5,5 @@
 std::vector<std::string> split(std::string str,std::string sep);
 std::string convert_to_lower(const std::string str);
 std::string route(const std::string request);
-
+void write_file(std::string str, std::string filename);
 #endif
