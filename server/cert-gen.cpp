62bcd10a5871e8f1db1336687b27b50dff2ba58f
fwh2110 <fwh2110@columbia.edu>
2020-12-12 02:52:58 +0000

    server side updates to sign csr and provide cert

---
+++ server/cert-gen.cpp
@@ -0,0 +1,185 @@
+#include <stdint.h>
+#include <stdio.h>
+#include <string>
+#include <openssl/err.h>
+#include <openssl/conf.h>
+#include <openssl/pem.h>
+#include <openssl/rand.h>
+#include <openssl/x509v3.h>
+#include <vector>
+#include "server_utils.h"
+
+int load_ca(const char *ca_key_path, EVP_PKEY **ca_key, const char *ca_crt_path, X509 **ca_crt)
+{
+	BIO *bio = NULL;
+	*ca_crt = NULL;
+	*ca_key = NULL;
+
+	/* Load CA public key. */
+	bio = BIO_new(BIO_s_file());
+	if (!BIO_read_filename(bio, ca_crt_path)) goto free;
+	*ca_crt = PEM_read_bio_X509(bio, NULL, NULL, NULL);
+	if (!*ca_crt) goto free;
+	BIO_free_all(bio);
+
+	/* Load CA private key. */
+	bio = BIO_new(BIO_s_file());
+	if (!BIO_read_filename(bio, ca_key_path)) goto free;
+	*ca_key = PEM_read_bio_PrivateKey(bio, NULL, NULL, NULL);
+	if (!ca_key) goto free;
+	BIO_free_all(bio);
+	return 1;
+free:
+	BIO_free_all(bio);
+	X509_free(*ca_crt);
+	EVP_PKEY_free(*ca_key);
+	return 0;
+}
+
+int generate_set_random_serial(X509 *crt)
+{
+	/* Generates a 20 byte random serial number and sets in certificate. */
+	unsigned char serial_bytes[20];
+	if (RAND_bytes(serial_bytes, sizeof(serial_bytes)) != 1) return 0;
+	serial_bytes[0] &= 0x7f; /* Ensure positive serial! */
+	BIGNUM *bn = BN_new();
+	BN_bin2bn(serial_bytes, sizeof(serial_bytes), bn);
+	ASN1_INTEGER *serial = ASN1_INTEGER_new();
+	BN_to_ASN1_INTEGER(bn, serial);
+
+	X509_set_serialNumber(crt, serial); // Set serial.
+
+	ASN1_INTEGER_free(serial);
+	BN_free(bn);
+	return 1;
+}
+
+int sign_csr(EVP_PKEY *ca_key, X509 *ca_crt, X509 **crt, X509_REQ *req)
+{
+	
+	EVP_PKEY *req_pubkey = NULL;
+	/* Sign with the CA. */
+	*crt = X509_new();
+	if (!*crt) goto free;
+
+	X509_set_version(*crt, 2); /* Set version to X509v3 */
+
+	/* Generate random 20 byte serial. */
+	if (!generate_set_random_serial(*crt)) goto free;
+
+	/* Set issuer to CA's subject. */
+	X509_set_issuer_name(*crt, X509_get_subject_name(ca_crt));
+
+	/* Set validity of certificate to 2 years. */
+	X509_gmtime_adj(X509_get_notBefore(*crt), 0);
+	X509_gmtime_adj(X509_get_notAfter(*crt), (long)2*365*24*3600);
+
+	/* Get the request's subject and just use it (we don't bother checking it since we generated
+	 * it ourself on the client side). Also take the request's public key. */
+	X509_set_subject_name(*crt, X509_REQ_get_subject_name(req));
+	req_pubkey = X509_REQ_get_pubkey(req);
+	X509_set_pubkey(*crt, req_pubkey);
+	EVP_PKEY_free(req_pubkey);
+
+	/* Now perform the actual signing with the CA. */
+	if (X509_sign(*crt, ca_key, EVP_sha256()) == 0) goto free;
+
+	X509_REQ_free(req);
+	return 1;
+
+free:
+	X509_REQ_free(req);
+	X509_free(*crt);
+	return 0;
+}
+
+void crt_to_pem(X509 *crt, uint8_t **crt_bytes, size_t *crt_size)
+{
+	/* Convert signed certificate to PEM format. */
+	BIO *bio = BIO_new(BIO_s_mem());
+	PEM_write_bio_X509(bio, crt);
+	*crt_size = BIO_pending(bio);
+	*crt_bytes = (uint8_t *)malloc(*crt_size + 1);
+	BIO_read(bio, *crt_bytes, *crt_size);
+	BIO_free_all(bio);
+}
+
+void print_bytes(uint8_t *data, size_t size)
+{
+	for (size_t i = 0; i < size; i++) {
+		printf("%c", data[i]);
+	}
+}
+
+int main(int argc, char *argv[])
+{
+	if(argc == 2){
+		//read first arg and decode as X509_REQ
+		std::string csrStr = argv[1];
+		std::vector<uint8_t> csrBytes = base64_decode(csrStr);
+		uint8_t *csr_data = csrBytes.data();
+
+		int csr_data_size = csrBytes.size();
+
+		BIO *bio = NULL;
+		X509_REQ* req = NULL;
+
+		// Create a read-only BIO backed by the supplied memory buffer
+		bio = BIO_new_mem_buf((void*)csr_data, csr_data_size);
+
+		PEM_read_bio_X509_REQ(bio, &req, NULL, NULL);
+
+		// Cleanup
+		BIO_free(bio);
+
+		const char *ca_key_path = "ca-key.pem";
+		const char *ca_crt_path = "ca-cert.pem";
+
+		/* Load CA key and cert. */
+		EVP_PKEY *ca_key = NULL;
+		X509 *ca_crt = NULL;
+		if (!load_ca(ca_key_path, &ca_key, ca_crt_path, &ca_crt)) {
+			fprintf(stderr, "Failed to load CA certificate and/or key!\n");
+			return 1;
+		}
+
+		/* Sign CSR */
+		X509 *crt = NULL;
+
+		int ret = sign_csr(ca_key, ca_crt, &crt, req);
+		if (!ret) {
+			fprintf(stderr, "Failed to generate key pair!\n");
+			return 1;
+		}
+
+        	BIO             *out = NULL, *bio_err = NULL;
+        	//todo this will be folder with cert
+		const char      *cPath = "client.pem";
+        	out = BIO_new_file(cPath,"w");
+        	ret = PEM_write_bio_X509(out, crt);
+
+		/* Convert key and certificate to PEM format. */
+		uint8_t *crt_bytes = NULL;
+		size_t crt_size = 0;
+
+		crt_to_pem(crt, &crt_bytes, &crt_size);
+		
+		std::string crt_str = base64_encode(crt_bytes, crt_size);
+		write_file(crt_str, "tmp-crt");
+		
+		/* Print key and certificate. */
+		print_bytes(crt_bytes, crt_size);
+
+		/* Free stuff. */
+		EVP_PKEY_free(ca_key);
+        	BIO_free_all(out);
+		X509_free(ca_crt);
+		X509_free(crt);
+		free(crt_bytes);
+
+		return 0;
+	}
+	else{
+		return 1;
+	}
+}
--- server/server_utils.cpp
+++ server/server_utils.cpp
@@ -84,3 +84,9 @@ std::string route(const std::string request)
 
     return response;
 }
+
+void write_file(std::string str, std::string filename){
+	std::ofstream file(filename);
+  	file << str;
+	file.close();
+}
